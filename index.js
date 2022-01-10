f7431ccafdebd9a825a9eb1718cb03392f9f95d8
Taylor Huffman <taylor@crucialartists.com>
2022-01-10 13:10:27 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const firstInitial = currentUser.slice(0, 1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
